ff795e8ca84da30483796cd4762543e15b543e5e
Mike Owino <mike@lancolatech.co.ke>
2022-03-30 17:21:10 +0300

    ...

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,44 @@
-/* https://www.youtube.com/watch?v=weZFfrjF-k4 */
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <link rel="icon" type="image/png" href="https://mikeowino.com/img/emojis/technologist.png" hreflang="en-us">
+  <title>Real Time</title>
+  <link rel="stylesheet" href="./style.css">
 
- const deg = 6;
-        const hr = document.querySelector('#hr');
-        const mn = document.querySelector('#mn');
-        const sc = document.querySelector('#sc');
-        
-        setInterval(() =>{
-            
-             let day = new Date();
-        let hh = day.getHours() * 30;
-        let mm = day.getMinutes() * deg;
-        let ss = day.getSeconds() * deg;
-        
-        hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
-        mn.style.transform = `rotateZ(${mm}deg)`;
-        sc.style.transform = `rotateZ(${ss}deg)`;
-            
-        })
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ url_for('static', filename='css/style.css') }}">
+    <title>Javascript Clock </title>
+</head>
+
+<body>
+
+    <div class="clock">
+
+        <div class="hour">
+            <div class="hr" id="hr"></div>
+        </div>
+
+        <div class="min">
+            <div class="mn" id="mn"></div>
+        </div>
+
+        <div class="sec">
+            <div class="sc" id="sc"></div>
+        </div>
+
+    </div>
+
+</body>
+</html>
+<!-- partial -->
+  <script  src="{{ url_for('static', filename='js/script.js') }}"></script>
+
+</body>
+</html>
